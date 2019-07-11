0ce493a7d3be9cae45a9a350da1bba9dbd93607b
kjoo <k-joo@fullout.jp>
2019-07-11 11:26:43 +0900

    udpate vue

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-
   resources :books
-
   namespace :api, format: 'json' do
     resources :books, only: [:index, :create, :update, :show, :destroy]
   end
-
-  post 'linebot/callback'
 end
